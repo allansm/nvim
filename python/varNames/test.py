5fcae8f6058391cac32c6c7f95e9604df3858466
allansm <allan.de.souza.melo.rj@gmail.com>
2021-05-06 19:14:59 -0300

    ...

--- python/varNames/test.py
+++ python/varNames/test.py
@@ -11,14 +11,17 @@ from fileHandle import *
 
 from os import chdir
 from os import getcwd
-from fn import *
+
+#edit this
+from test2 import *
+
 from subprocess import call
 
 import argparse
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument("code",type=str)
-parser.add_argument("word",type=str)
+#parser.add_argument("word",type=str)
 
 code = parser.parse_args().code
 #match = parser.parse_args().word
@@ -31,5 +34,38 @@ fold = ls(".","*")
 variables = ""
 
 for f in fold:
-    
+    if(os.path.isfile(f)):
+        #print(f)
+        var = getVariables(f,"",";")
+        if(var != "" or not var):
+            variables = variables+";"+var
+    else:
+        chdir(f)
+        
+        files = ls(".","*.*")
+
+        for fi in files:
+            #print(fi)
+            try:
+                var = getVariables(fi,"",";")
+                if(var != "" or not var):
+                    variables = variables+";"+var
+            except:
+                dummy = ""
+                print("error")
+            
+        
+        chdir("..")
+
+#variables = variables.replace(";","")
+#variables = removeBreakLine(variables)
+arr = variables.split(";")
+arr = list(dict.fromkeys(arr))
+arr2 = []
+for val in arr:
+    arr2.append(val.split("=")[0])
+
+#del variables
+variables = " "
+variables = variables.join(arr2)
 
